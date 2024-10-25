2993557fd6b025c4cf32f88d0d3b1747e8f81b18
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-10-25 17:29:20 +0530

    Simple example of Book class using fun()

--- lib/main.dart
+++ lib/main.dart
@@ -1,36 +1,18 @@
-import 'package:flutter/material.dart';
+class Book {
+  String name;
+  int price;
+  String author;
+  Book(this.name, this.price, this.author);
+
+  void detail() {
+    print("Book Name: $name");
+    print("Book Price: $price");
+    print("Author Name: $author");
+  }
+}
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MapExampleScreen(),
-    );
-  }
-}
-
-class MapExampleScreen extends StatelessWidget {
-  // Define a list of numbers
-  final List<int> numbers = [1, 2, 3, 4, 5];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Map Function Example'),
-      ),
-      body: ListView(
-        children: numbers
-            .map((number) => ListTile(
-                  title: Text('Number: $number'),
-                  leading: Icon(Icons.format_list_numbered),
-                ))
-            .toList(),
-      ),
-    );
-  }
+  Book m = Book("Java", 890, "jjj");
+  m.detail();
+  print(m);
 }
