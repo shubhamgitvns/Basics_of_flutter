3528b56bf1f6eefe79b64516c213af0865b1bae2
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-11-05 14:49:52 +0530

    The simple example of two class the class a extends in class b

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,51 @@
-class A {
+class Person {
   String name;
-  void f1() {
-    print("f1() in a");
-  }
-
-  void f2() {
-    print("f2() in a");
-  }
-
-  A(this.name);
+  int age;
+  String address;
+  String mob_number;
+  // constructor
+  Person({
+    required this.name, // Required Parameter
+    required this.age, // Required Parameter
+    required this.address,
+    required this.mob_number,
+  });
+// to String method
+  @override
   String toString() {
-    return "$name";
+    return 'Person(Name: $name, Age: $age, Address: $address, Number: $mob_number)';
   }
 }
 
-class B extends A {
+class Employee extends Person {
   String post;
+  int salary;
 
-  void f2() {
-    print("f2() in b");
-  }
+  //Person({required super.name, required super.age, required super.address, required super.mob_number});
 
-  B(name, this.post) : super(name);
+  Employee(
+      {required name,
+      required age,
+      required address,
+      required mob_number,
+      required this.post,
+      required this.salary})
+      : super(name: name, age: age, address: address, mob_number: mob_number);
+  @override
   String toString() {
-    return "$name,$post";
+    return '${super.toString()},Employee(Post: $post, Salary: $salary)';
   }
 }
 
 void main() {
-  A x1 = A('Popat');
-  x1.f2();
-  x1 = B('Popat', "Ustad");
-  x1.f2();
+  //Class calling
+  Person x2 = Employee(
+      name: 'Shubham',
+      age: 22,
+      address: "j1/112b",
+      mob_number: "9066854367",
+      post: 'ceo',
+      salary: 6887);
+  // where m is instance of class
+  print(x2);
 }
