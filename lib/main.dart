09ac2037ccff7fce0b228102a8754ff7c78e2986
shubhamgitvns <shubhammaurya10092002@gmail.com>
2024-11-06 16:07:12 +0530

    The default parameter features of dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,10 @@
-class A {
-  String name;
-  void f1() {
-    print("f1() in a");
-  }
-
-  void f2() {
-    print("f2() in a");
-  }
-
-  void calculateArea(int length, int width) {
-    print('Area is ${length * width}');
-  }
-
-  A(this.name);
-  String toString() {
-    return "$name";
-  }
-}
-
-class B extends A {
-  String post;
-
-  void f2() {
-    print("f2() in b");
-  }
-
-  B(name, this.post) : super(name);
-  String toString() {
-    return "$name,$post";
-  }
-}
-
 void main() {
-  A x1 = A('Popat');
-  x1.f2();
-  x1 = B('Popat', "Ustad");
-  x1.f2();
+  add(a: 4, b: 5);
+  add();
+  add(a: 9);
+  add(b: 8);
+}
+
+void add({int a = 1, int b = 2}) {
+  print(a + b);
 }
